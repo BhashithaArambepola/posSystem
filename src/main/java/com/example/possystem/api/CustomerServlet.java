0e2334534f2cad956cb45f209f78cda4c62dd7db
Bhashitha Arambepola <barambepola@gmail.com>
2022-05-02 09:53:54 +0530

    add besic validation to CustomerServlet

--- src/main/java/com/example/possystem/api/CustomerServlet.java
+++ src/main/java/com/example/possystem/api/CustomerServlet.java
@@ -1,5 +1,9 @@
 package com.example.possystem.api;
 
+import com.example.possystem.dto.CustomerDTO;
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -7,13 +11,20 @@ import java.io.IOException;
 
 @WebServlet(name = "CustomerServlet", value = {"/CustomerServlets","/CustomerServlets/"})
 public class CustomerServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("working..");
+        if(request.getContentType()== null || !request.getContentType().startsWith("application/json") ){
+            response.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE,"Less Content");
+            return;
+        }
+        if(request.getPathInfo() !=null && !request.getPathInfo().equals("/")){
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+        Jsonb jsonb = JsonbBuilder.create();
+        CustomerDTO customer = jsonb.fromJson(request.getReader(), CustomerDTO.class);
+
     }
 }
